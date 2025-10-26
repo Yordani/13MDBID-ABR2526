9dc39c45cc7e44913c1d75694738bba8dcc42475
Yordani <yordani2007@gmail.com>
2025-10-26 08:34:39 -0400

    Inicio de la SP4 Generacion de test basicos con pytest

--- test/test_data_quality.py
+++ test/test_data_quality.py
@@ -8,6 +8,7 @@ def datos_banco():
     return df
 
 def test_esquema():
+    ''' Prueba para verificar que el DataFrame cumple con el esquema esperado '''
     df = datos_banco
     esquena = DataFrameSchema({
         "age": Column(int, nullable=False),
@@ -22,3 +23,25 @@ def test_esquema():
     df = pd.read_csv('data/raw/bank-additional-full.csv', sep=';')
 
     esquena.validate(df)
+
+def test_basico(datos_banco):
+    ''' Prueba básica para verificar que el DataFrame no está vacío y tiene las columnas esperadas '''
+    df = datos_banco
+
+    #Verificar que el DataFrame no está vacío
+    assert not df.empty, "El DataFrame está vacío"
+    #verificar que no hay valores nulos
+    assert df.isnull().sum().sum() == 0, "El DataFrame no contiene valores nulos"
+    #Verificar que el DataFrame tiene el número esperado de columnas
+    assert df.shape[1] == 21, "El DataFrame no tiene filas"
+
+if __name__ == "__main__":
+    try:
+        test_esquema(datos_banco())
+        test_basico(datos_banco())
+        print("Todas las pruebas pasaron exitosamente.")
+        with open("docs/test_results/test_results.txt", "w") as f:
+            f.write("Todas las pruebas pasaron exitosamente.\n")
+    except AssertionError as e:
+         with open("docs/test_results/test_results.txt", "w") as f:
+            f.write(f"Fallo en la prueba: {e}\n")
